8bfca8f46b3c13d59e5b7fe2544a77bf0e0737cb
MariuszKraw <#Jozue00GH>
2021-11-25 10:47:28 +0100

    wersja 2

--- src/pl/softronic/szkolenia/java/gielda/Gielda.java
+++ src/pl/softronic/szkolenia/java/gielda/Gielda.java
@@ -4,13 +4,27 @@ import java.util.SplittableRandom;
 
 public class Gielda {
 
-    String nazwa = "";
-    String adres = "";
+    private String nazwa = "";
+    protected String adres = "";
 
     public static void main(String[] args) {
         Kontroler kontroler = new Kontroler();
         kontroler.uruchomProgram();
     }
+// krok trzeci
+    public String getNazwa() {
+        return nazwa;
+    }
 
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
 
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
 }
--- src/pl/softronic/szkolenia/java/gielda/Kontroler.java
+++ src/pl/softronic/szkolenia/java/gielda/Kontroler.java
@@ -1,6 +1,9 @@
 package pl.softronic.szkolenia.java.gielda;
 
 public class Kontroler {
+// krok 2
+    Gielda gielda;
+
     public void uruchomProgram() {
         //Uruchamia metody: tworzącą i testujacą giełdę
 
@@ -15,8 +18,10 @@ public class Kontroler {
 
     // mariusz 
     public void tworzGielde() {
-
         System.out.println("Tworzę giełdę");
+
+        gielda = new Gielda();
+        gielda.setNazwa("Gielda MK");
     }
 
     public void testujGielde(){
